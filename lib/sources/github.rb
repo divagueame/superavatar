e27c7868589e177697c43a7a4107f8e80799cabd
Martin Arce <martin.arce.teixeira@gmail.com>
2023-04-07 23:33:58 +0200

    Add Github endpoint

--- lib/sources/github.rb
+++ lib/sources/github.rb
@@ -1,9 +1,22 @@
+
 module Superavatar
   module Sources
     class Github
-      def self.profile_url(username)
-        'https://github.com/divagueame/'
+
+      def avatar_url(username)
+        res = profile(username)
+        JSON.parse(res)[:avatar_url]
       end
+
+      private
+      def self.profile_url(username)
+        "https://github.com/#{username}/"
+      end
+
+      def profile(username)
+        "https://api.github.com/users/#{username}/"
+      end
+
     end
   end
 end
--- superavatar.gemspec
+++ superavatar.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
     s.name        = "superavatar"
-    s.version     = "0.0.0"
+    s.version     = "0.0.1"
     s.summary     = "Your app has users but no avatar. Let's fix that"
     s.description = "A gem to get your users' backup avatar from public resources"
     s.authors     = ["Martin Arce"]
